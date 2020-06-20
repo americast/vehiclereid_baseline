ad68ac8e0fb67eab23b8a6d7e008758dc47cf68c
Sayan Sinha <sayan.sinha@iitkgp.ac.in>
2020-06-20 13:32:06 +0530

    Add num_veri to eval.sh

--- eval.sh
+++ eval.sh
@@ -10,5 +10,5 @@ galleryList_veri="./list/veri_test_list.txt"
 # Number of classes
 num_veri=576
 
-CUDA_VISIBLE_DEVICES=1 python -u eval.py $queryPath_veri $queryList_veri $galleryPath_veri $galleryList_veri --dataset veri --backbone resnet50 --weights './models/resnet50/Car_epoch_50.pth' --save_dir './results/veri/resnet50/'
+CUDA_VISIBLE_DEVICES=1 python -u eval.py $queryPath_veri $queryList_veri $galleryPath_veri $galleryList_veri -n $num_veri --dataset veri --backbone resnet50 --weights './models/resnet50/Car_epoch_50.pth' --save_dir './results/veri/resnet50/'
 
